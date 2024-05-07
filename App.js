f563cc5335b80136c55e59975310a11e54aa8979
Tim Etler <tmetler@gmail.com>
2024-05-06 21:09:59 -0700

    Expo dev networking streaming blocking bug demo

--- App.js
+++ App.js
@@ -1,10 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+const applyGlobalPolyfills = () => {
+  const {
+    polyfillGlobal,
+  } = require("react-native/Libraries/Utilities/PolyfillFunctions");
+  const { TextEncoder, TextDecoder } = require("text-encoding");
+  const {
+    fetch,
+    Headers,
+    Request,
+    Response,
+  } = require("react-native-fetch-api");
+  const {
+    ReadableStream,
+    TransformStream,
+  } = require("web-streams-polyfill/ponyfill/es6");
+  const { TextDecoderStream } = require("@stardazed/streams-text-encoding");
+
+  polyfillGlobal("TextEncoder", () => TextEncoder);
+  polyfillGlobal("TextDecoder", () => TextDecoder);
+
+  polyfillGlobal("fetch", () => fetch);
+  polyfillGlobal("Headers", () => Headers);
+  polyfillGlobal("Request", () => Request);
+  polyfillGlobal("Response", () => Response);
+
+  polyfillGlobal("ReadableStream", () => ReadableStream);
+  polyfillGlobal("TransformStream", () => TransformStream);
+
+  polyfillGlobal("TextDecoderStream", () => TextDecoderStream);
+};
+
+applyGlobalPolyfills();
+
+// Replace this URL with the URL of the server. Ngrok may be needed for the preview build.
+const url = "http://192.168.1.1:3000";
 
 export default function App() {
+  const [headers, setHeaders] = React.useState("");
+  const [body, setBody] = React.useState("");
+  const [error, setError] = React.useState("");
+  const [isStreaming, setIsStreaming] = React.useState(false);
+  const startStream = async () => {
+    try {
+      const response = await fetch(url, {
+        reactNative: { textStreaming: true },
+      });
+      setHeaders(
+        JSON.stringify(Object.fromEntries(response.headers.entries()), null, 2)
+      );
+      const stream = response.body.pipeThrough(new TextDecoderStream());
+      const reader = stream.getReader();
+      let buffer = "";
+      let readResult;
+      do {
+        readResult = await reader.read();
+        buffer += readResult.value ?? "";
+        setBody(buffer);
+      } while (!readResult.done);
+    } catch (error) {
+      setError(error.toString());
+    } finally {
+      setIsStreaming(false);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Start Stream"
+        onPress={() => {
+          setHeaders("");
+          setBody("");
+          setError("");
+          setIsStreaming(true);
+          startStream();
+        }}
+        disabled={isStreaming}
+      />
+      <Text>Response Headers: {headers}</Text>
+      <Text>Response Body: {body}</Text>
+      <Text>Fetch Error: {error}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +90,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ server.js
@@ -0,0 +1,24 @@
+const { Readable } = require("node:stream");
+const { setTimeout } = require("node:timers/promises");
+const Koa = require("koa");
+
+const port = process.env.PORT ?? 3000;
+
+const app = new Koa();
+
+app.use(async (ctx) => {
+  const responseStream = new Readable({ read() {} });
+  ctx.type = "text/plain";
+  ctx.body = responseStream;
+  (async () => {
+    for (let i = 0; i < 10; i++) {
+      await setTimeout(1000);
+      responseStream.push(`${i}`);
+    }
+    responseStream.push(null);
+  })();
+});
+
+app.listen(port);
+
+console.info(`Server started on port ${port}`);
